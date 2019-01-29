9ea3586af41c7f610c4a7189bc5821d5b0d99891
muffy <marufeuille@gmail.com>
2019-01-29 23:21:49 +0900

    use mkdocs

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@
 git clone ${GIT_REPO} /root/repos
 cd /root/repos
 git checkout ${SOURCE_BRANCH}
-hugo
-cp CNAME public/
-ghp-import -b master -m "$(date '+%Y%m%d-%h%M%s') commit" public/
+mkdocs build
+cp CNAME site/
+ghp-import -b master -m "$(date '+%Y%m%d-%h%M%s') commit" site/
 git push origin master
